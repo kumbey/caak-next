358f56cfaa7c90ab1c5a95b9370a55bb4a3ca56e
nano <nanoshdee@gmail.com>
2021-11-10 00:19:42 +0800

    a

--- next.config.js
+++ next.config.js
@@ -3,6 +3,5 @@ module.exports = {
   images: {
     loader: 'imgix',
     path: 'https://www.elegantthemes.com/blog/wp-content/uploads/2019/10/loading-screen-featured-image.jpg',
-  },
-  webpack5: false,
+  }
 }

37ff552fb35b3b2af74170de39f6ed8befe3e726
kumurbek.b <kumurbek.b@gmail.com>
2021-12-04 17:57:23 +0800

    added: group sort and view buttons

--- src/components/navigation/sortButtonTypes.js
+++ src/components/navigation/sortButtonTypes.js
@@ -33,3 +33,35 @@ export const searchResultType = [
     icon: "icon-fi-rs-group",
   },
 ];
+
+export const GroupType = [
+  {
+    id: 0,
+    type: "Трэнд",
+    icon: "icon-fi-rs-trend",
+  },
+  {
+    id: 1,
+    type: "Шинэ",
+    icon: "icon-fi-rs-new",
+  },
+  {
+    id: 2,
+    type: "Шилдэг",
+    icon: "icon-fi-rs-flash",
+  },
+];
+export const GroupViewType = [
+  {
+    id: 0,
+    icon: "icon-fi-rs-new",
+
+    // icon: "icon-fi-rs-list-card",
+  },
+  {
+    id: 1,
+    icon: "icon-fi-rs-flash",
+
+    // icon: "icon-fi-list-grid",
+  },
+];

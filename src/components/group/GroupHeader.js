b21446fc8cdfbe8fbd435a7d9493ab69a8064bb1
kumurbek.b <kumurbek.b@gmail.com>
2021-12-04 17:56:28 +0800

    added group page

---
+++ src/components/group/GroupHeader.js
@@ -0,0 +1,148 @@
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Button from "../button";
+import profImg from "../../../public/assets/images/logo.png";
+import { graphqlOperation } from "@aws-amplify/api-graphql";
+import API from "@aws-amplify/api";
+import { useUser } from "../../context/userContext";
+import {
+  createGroupUsers,
+  deleteGroupUsers,
+} from "../../graphql-custom/GroupUsers/mutation";
+import DropDown from "../navigation/DropDown";
+import PostMoreMenu from "../card/PostMoreMenu";
+import { getFileUrl } from "../../utility/Util";
+
+const GroupHeader = ({ groupData, ...props }) => {
+  const { isLogged, cognitoUser } = useUser();
+  const [loading, setLoading] = useState(false);
+  const [forceRender, setForceRender] = useState(0);
+
+  const followGroup = async () => {
+    try {
+      if (isLogged) {
+        setLoading(true);
+        if (groupData.followed) {
+          await API.graphql(
+            graphqlOperation(deleteGroupUsers, {
+              input: {
+                id: `${groupData.id}#${cognitoUser.attributes.sub}`,
+              },
+            })
+          );
+          groupData.followed = false;
+          groupData.totals.member -= 1;
+          setForceRender(forceRender + 1);
+        } else {
+          await API.graphql(
+            graphqlOperation(createGroupUsers, {
+              input: {
+                id: `${groupData.id}#${cognitoUser.attributes.sub}`,
+                group_id: groupData.id,
+                user_id: cognitoUser.attributes.sub,
+                role: "MEMBER",
+              },
+            })
+          );
+          groupData.followed = true;
+          groupData.totals.member += 1;
+          setForceRender(forceRender + 1);
+        }
+        setLoading(false);
+      }
+    } catch (ex) {
+      setLoading(false);
+      console.log(ex);
+    }
+  };
+
+  return (
+    <div className={"flex flex-col items-center justify-center  bg-white"}>
+      <div className="lg:w-[966px] w-auto sm:flex sm:justify-between justify-evenly mb-[20px]">
+        <div className="flex">
+          <div className="w-[148px] h-[148px] rounded-3xl border-8 border-white mr-[20px]">
+            <Image
+              className=" bg-white rounded-3xl"
+              src={getFileUrl(groupData.cover)}
+              width={148}
+              height={148}
+              layout="responsive"
+              //   objectFit={"cover"}
+              alt="#"
+            />
+          </div>
+          <div className="flex flex-col mt-[73px]">
+            <div>
+              <h1 className="text-24px font-bold">{groupData.name}</h1>
+            </div>
+            <div className="flex">
+              <div className="flex mr-[22px] items-center">
+                <span className={"icon-fi-rs-aura mr-1"} />
+                <p className="text-sm">2434 Аура</p>
+              </div>
+              <div className="flex mr-[22px] items-center">
+                <span className={"icon-fi-rs-globe mr-1"} />
+                <p className="text-sm">Нээлттэй бүлэг</p>
+              </div>
+              <div className="flex items-center">
+                <span className={"icon-fi-rs-group-o mr-1"} />
+                <p className="text-sm">{groupData.totals.member} Гишүүн</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-end mt-[10px] sm:mt-[73px]">
+          {groupData.role_on_group === "ADMIN" ? (
+            <Button
+              className="h-[36px] w-[124px] rounded-lg text-white text-base font-medium font-inter"
+              icon={<span className="icon-fi-rs-settings-f  mr-1" />}
+              iconPosition="left"
+              // onClick={handleClick}
+              skin={"bg-caak-cardinal"}
+            >
+              Тохиргоо
+            </Button>
+          ) : (
+            <>
+              <Button
+                loading={loading}
+                className="h-[36px] w-[124px] rounded-lg text-white text-base font-medium font-inter"
+                icon={<span className="icon-fi-rs-add-group-f  mr-1" />}
+                iconPosition="left"
+                onClick={followGroup}
+                skin={`${
+                  groupData.followed
+                    ? "bg-caak-generalblack"
+                    : "bg-caak-cardinal"
+                }`}
+              >
+                {groupData.followed ? "Нэгдсэн" : "Нэгдэх"}
+              </Button>
+              <div
+                // ref={menuRef}
+                // onClick={toggleMenu}
+                className={`flex justify-center flex-shrink-0 ml-3 w-[35px] h-[35px] transition ease-linear duration-100 items-center cursor-pointer relative bg-caak-extraLight hover:bg-caak-titaniumwhite rounded-full`}
+              >
+                <span className="icon-fi-rs-dots text-22px" />
+                {/* <DropDown
+                // open={isMenuOpen}
+                // onToggle={toggleMenu}
+                content={
+                  <PostMoreMenu
+                    groupId={groupData.id}
+                    // postId={post.id}
+                    // postUser={post.user}
+                  />
+                }
+                className={"top-6 -right-3"}
+              /> */}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GroupHeader;

f30644d34633e38d936c758102d23b149d3eb26d
kumurbek.b <kumurbek.b@gmail.com>
2021-12-06 00:29:34 +0800

    added: comment

--- src/graphql-custom/comment/queries.js
+++ src/graphql-custom/comment/queries.js
@@ -1,5 +1,6 @@
 import comment0001 from "./fields/comment0001";
 import comment0002 from "./fields/comment0002";
+import comment0003 from "./fields/comment0003";
 
 export const getCommentsByPostItem = /* GraphQL */ `
     query GetCommentsByPostItem(
@@ -26,5 +27,24 @@ export const getComment = /* GraphQL */ `
     query GetComment($id: ID!) 
     {
         getComment(id: $id) ${comment0001}
+    }
+`;
+
+export const listCommentByUser = /* GraphQL */ `
+    query ListCommentByUser(
+        $user_id: ID!
+        $sortDirection: ModelSortDirection
+        $nextToken: String
+    ) 
+    {
+        listCommentByUser
+    (     
+            user_id: $user_id
+            sortDirection: $sortDirection
+            nextToken: $nextToken
+    )   {
+        items ${comment0003}
+        nextToken
+    }
     }
 `;

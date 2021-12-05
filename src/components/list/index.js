14bb8d796fddcf181ed884adc22f2518ef3d32a7
kumurbek.b <kumurbek.b@gmail.com>
2021-12-06 00:32:25 +0800

    updated: list component

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import { useEffect } from "react";
-import { getFileUrl } from "../../utility/Util";
+import {
+  generateTimeAgo,
+  getFileUrl,
+  getGenderImage,
+} from "../../utility/Util";
 
 const List = ({ video, post, imageSrc, ...props }) => {
-  useEffect(() => {
-    console.log(imageSrc);
-  }, []);
   return (
     <div className="bg-white relative flex   mx-auto  rounded-lg shadow-card  mb-[24px]">
       <div className="flex m-[15px]">
@@ -13,7 +13,7 @@ const List = ({ video, post, imageSrc, ...props }) => {
           <div className={"w-[96px] h-[96px] relative"}>
             <Image
               className=" bg-white rounded-3xl"
-              src={getFileUrl(imageSrc)}
+              src={!imageSrc ? getGenderImage("default") : getFileUrl(imageSrc)}
               width={96}
               height={96}
               layout="responsive"
@@ -27,21 +27,21 @@ const List = ({ video, post, imageSrc, ...props }) => {
             {post.title}
           </div>
           <div className="flex mt-1 text-xs text-caak-darkBlue font-normal font-inter">
-            <div className="">Кино сонирхогчид</div>
-            <div className="">@undercoverosh</div>
-            <div className="">24 мин</div>
+            <div className="mr-[23px]">{post.group.name}</div>
+            <div className="mr-[24px]">@{post.user.nickname}</div>
+            <div className="">{generateTimeAgo(post.createdAt)}</div>
           </div>
           <div className="flex mt-2">
-            <div className="flex items-center mr-6">
-              <span className="icon-fi-rs-rock-i text-2xl mr-2" />
+            <div className="flex items-center mr-6 ">
+              <span className="icon-fi-rs-rock-i text-2xl mr-2 cursor-pointer" />
               <p>23</p>
             </div>
             <div className="flex items-center mr-6">
-              <span className="icon-fi-rs-comment-f text-2xl mr-2" />
+              <span className="icon-fi-rs-comment-f text-2xl mr-2 cursor-pointer" />
               <p>14</p>
             </div>
             <div className="flex items-center mr-6">
-              <span className="icon-fi-rs-share text-2xl mr-2" />
+              <span className="icon-fi-rs-share text-2xl mr-2 cursor-pointer" />
             </div>
           </div>
         </div>

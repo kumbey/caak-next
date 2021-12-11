ee0b8450d7e3d87c123872ef7144746c47ccaed1
kumurbek.b <kumurbek.b@gmail.com>
2021-12-12 00:39:34 +0800

    added: delete comment

--- src/components/list/CommentList.js
+++ src/components/list/CommentList.js
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { extractDate, getFileUrl, getGenderImage } from "../../utility/Util";
 import Image from "next/image";
 import Button from "../../components/button";
 import Divider from "../divider";
+import { useUser } from "../../context/userContext";
+import { API } from "aws-amplify";
+import { deleteComment } from "../../graphql-custom/comment/mutation";
 
 const CommentList = ({ comment, imageSrc, ...props }) => {
+  const { isLogged } = useUser();
+  const [loading, setLoading] = useState(false);
+  console.log(comment.sub.items);
   const createdAt = extractDate(comment.createdAt);
+
+  const deleteComments = async (id) => {
+    if (isLogged)
+      try {
+        setLoading(true);
+        console.log("started call api");
+        await API.graphql({
+          query: deleteComment,
+          variables: {
+            input: {
+              id: id,
+            },
+          },
+        });
+        console.log("after  api");
+
+        setLoading(false);
+      } catch (ex) {
+        setLoading(false);
+        console.log(ex);
+      }
+  };
+
+  const handleDelete = async () => {
+    console.log("delete");
+
+    if (comment.sub.items.length === 0) {
+      console.log("no sub");
+      await deleteComments(comment.id);
+    } else {
+      console.log("has sub");
+      comment.sub.items.map((sub, index) => {
+        deleteComments(sub.id);
+      });
+      await deleteComments(comment.id);
+    }
+  };
+
   return (
     <div className="  w-full">
       <div className="relative flex items-center ">
@@ -57,7 +101,8 @@ const CommentList = ({ comment, imageSrc, ...props }) => {
         </div>
         <div className="flex mr-[10px]">
           <Button
-            onClick={() => null}
+            onClick={() => handleDelete()}
+            loading={loading}
             round
             className={
               "hover:bg-gray-100 border border-gray-200 w-[102px] h-[39px]  font-medium font-inter rounded-lg text-caak-generalblack text-15px bg-white relative"

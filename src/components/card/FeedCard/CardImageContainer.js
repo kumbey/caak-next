c51ecc4d4577f53c70b4ec5c88bc8647eb6e27b7
weahforsage <sanjasapple@gmail.com>
2021-12-21 16:56:13 +0800

    fix: feed image sizing

--- src/components/card/FeedCard/CardImageContainer.js
+++ src/components/card/FeedCard/CardImageContainer.js
@@ -4,19 +4,17 @@ import Image from "next/image";
 const CardImageContainer = ({ file }) => {
   return (
     <div className={"relative w-full max-w-[1400px] mx-auto h-full"}>
-      <div className={"relative w-full h-full"}>
+      <div className={"relative w-full h-full unset-img"}>
         <Image
           placeholder={"blur"}
           blurDataURL={getFileUrl(file)}
           alt={file.name}
           src={getFileUrl(file)}
-          className={"w-full h-full max-h-[770px]"}
+          className={"w-full h-full custom-img"}
           layout={"fill"}
-          objectFit={"contain"}
         />
       </div>
     </div>
-    // {/*</div>*/}
   );
 };
 

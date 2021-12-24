e0cae270b6457d75236ba1199cce882ac13f1dd8
nano <nanoshdee@gmail.com>
2021-12-25 04:31:15 +0800

    Update redirectUrls.js

--- src/redirectUrls.js
+++ src/redirectUrls.js
@@ -1,6 +1,6 @@
 const RedirectUrls = {
-    redirectSignIn: "https://localhost:3000/signInUp/federated/hook",
-    redirectSignOut: "https://localhost:3000/"
+    redirectSignIn: "https://main.d36nkdl3p18em1.amplifyapp.com/signInUp/federated/hook",
+    redirectSignOut: "https://main.d36nkdl3p18em1.amplifyapp.com/signInUp/federated/hook"
 }
 
 export default RedirectUrls

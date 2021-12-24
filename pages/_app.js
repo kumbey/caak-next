a06c1c3726bebf1edf1c2e74851f7d78c7c48542
nano <nanoshdee@gmail.com>
2021-12-24 17:17:35 +0800

    a

--- pages/_app.js
+++ pages/_app.js
@@ -11,12 +11,52 @@ import "nprogress/nprogress.css"; //styles of nprogress
 import Router from "next/router";
 import ViewPostModal from "../src/components/modals/ViewPostModal";
 import ViewPostItemModal from "../src/components/modals/viewPostItemModal";
+import Configure from "../src/configure";
 
-Amplify.configure({ ...awsExports, ssr: true });
-Storage.configure({ level: "public" });
+if(typeof window !== 'undefined'){
+  const isLocalhost = Boolean(
+    window.location.hostname === "localhost" ||
+      // [::1] is the IPv6 localhost address.
+      window.location.hostname === "[::1]" ||
+      // 127.0.0.1/8 is considered localhost for IPv4.
+      window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+      )
+  );
+  
+  const isDevelopment = Boolean(
+    window.location.hostname === "https://main.d36nkdl3p18em1.amplifyapp.com/"
+  );
+  
+  // Assuming you have two redirect URIs, and the first is for localhost and second is for production
+  const [
+    localRedirectSignIn,
+    productionRedirectSignIn,
+  ] = Configure.redirectSignIn.split(",");
+  
+  const [
+    localRedirectSignOut,
+    productionRedirectSignOut,
+  ] = Configure.redirectSignOut.split(",");
+  
+  const updatedAwsConfig = {
+    ...awsExports,
+    oauth: {
+      ...awsExports.oauth,
+      redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+      redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
+    },
+    ssr: true
+  }
+  
+  Amplify.configure(updatedAwsConfig);
+  Storage.configure({ level: "public" });
+}
+
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
+
 
 function MyApp({ Component, pageProps }) {
   return (

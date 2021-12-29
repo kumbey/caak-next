030bc0fea1e98125bab19c2e2b3dbd1babcaf1ee
weahforsage <sanjasapple@gmail.com>
2021-12-29 14:46:10 +0800

    add: blog view comment reply input

--- src/components/card/CommentItemCard.js
+++ src/components/card/CommentItemCard.js
@@ -7,16 +7,21 @@ import {
 import AnimatedCaakButton from "../button/animatedCaakButton";
 import ProfileHoverCard from "./ProfileHoverCard";
 import Tooltip from "../tooltip/Tooltip";
+import ViewPostBlogAddComment from "../input/ViewPostBlogAddComment";
+import { useEffect, useState } from "react";
 
 const CommentItemCard = ({
   children,
   subComment,
   comment,
-  setReply,
-  setCommentInputValue,
-  addCommentRef,
   jumpToCommentId,
+  postId
 }) => {
+  const [isReplyInputActive, setIsReplyInputActive] = useState(false);
+  const [reply, setReply] = useState({
+    isReplying: true
+  });
+
   return (
     <div
       className={`flex flex-row justify-between ${subComment ? "" : ""} w-full`}
@@ -46,7 +51,7 @@ const CommentItemCard = ({
             jumpToCommentId === comment.id ? "commentFade" : ""
           }`}
         >
-          <div className={"mb-[4px]"}>
+          <div className={"mb-[4px] w-max"}>
             <Tooltip
               className={"-left-6"}
               content={<ProfileHoverCard userId={comment.user.id} />}
@@ -60,78 +65,77 @@ const CommentItemCard = ({
               </p>
             </Tooltip>
           </div>
-
-          <div className={"flex flex-row items-center justify-between"}>
-            <div className={"flex flex-col justify-center"}>
-              <p
-                className={`text-caak-generalblack text-[15px] tracking-[0.23px] leading-[18px] break-all`}
-              >
-                {comment.comment}
-              </p>
-              <div
-                className={
-                  "flex flex-row text-caak-darkBlue items-center mt-[10px]"
-                }
-              >
-                <div>
-                  <p className={"text-[13px]"}>
-                    {generateTimeAgo(comment.createdAt)}
-                  </p>
-                </div>
-                <div
-                  onClick={() => {
-                    setCommentInputValue(
-                      (prev) => {
-                       if(!prev?.startsWith(`@${comment.user.nickname}`)){
-                        return `@${comment.user.nickname} ${prev}`
-                       }
-                       else {
-                         return prev
-                       }
-                      }
-                    );
-                    setReply({
-                      isReplying: true,
-                      user_id: comment.user.id,
-                      user_nickname: `@${comment.user.nickname} `,
-                      comment_id: subComment ? subComment : comment.id,
-                    });
-                    if (addCommentRef?.current){
-                      addCommentRef.current.focus()
-                    }
-                  }}
-                  className={"flex flex-row item-center ml-[16px]"}
+          <div className={"flex flex-col"}>
+            <div className={"flex flex-row items-center justify-between"}>
+              <div className={"flex flex-col justify-center w-full"}>
+                <p
+                  className={`text-caak-generalblack text-[15px] tracking-[0.23px] leading-[18px] break-all`}
                 >
-                  <div
-                    className={
-                      "flex items-center justify-center w-[18px] h-[18px] cursor-pointer"
-                    }
-                  >
-                    <span className={"icon-fi-rs-comment-o text-[15px]"} />
+                  {comment.comment}
+                </p>
+                <div
+                  className={
+                    "flex flex-row text-caak-darkBlue items-center mt-[10px]"
+                  }
+                >
+                  <div>
+                    <p className={"text-[13px]"}>
+                      {generateTimeAgo(comment.createdAt)}
+                    </p>
                   </div>
-                  <p className={"text-[13px] cursor-pointer"}>Хариулах</p>
+                  <div
+                    onClick={() => {
+                      setIsReplyInputActive(true);
+                      setReply({
+                        isReplying: true,
+                        user_id: comment.user.id,
+                        user_nickname: `@${comment.user.nickname} `,
+                        comment_id: subComment ? subComment : comment.id,
+                      });
+                    }}
+                    className={"flex flex-row item-center ml-[16px]"}
+                  >
+                    <div
+                      className={
+                        "flex items-center justify-center w-[18px] h-[18px] cursor-pointer"
+                      }
+                    >
+                      <span className={"icon-fi-rs-comment-o text-[15px]"} />
+                    </div>
+                    <p className={"text-[13px] cursor-pointer"}>Хариулах</p>
+                  </div>
                 </div>
               </div>
+              <div className={"self-start"}>
+                <AnimatedCaakButton
+                  reactionType={"COMMENT"}
+                  itemId={comment.id}
+                  totals={comment.totals}
+                  reacted={comment.reacted}
+                  setReacted={(changedReacted) => {
+                    comment.reacted = changedReacted;
+                  }}
+                  hideCaakText
+                  bottomTotals
+                  textClassname={
+                    "text-[13px] font-medium text-13px tracking-[0.2px] leading-[16px] text-caak-nocturnal"
+                  }
+                  iconContainerClassname={"w-[24px] h-[24px] bg-transparent"}
+                  iconColor={"text-caak-nocturnal"}
+                  iconClassname={"text-[23px]"}
+                />
+              </div>
             </div>
-            <div className={"self-start"}>
-              <AnimatedCaakButton
-                reactionType={"COMMENT"}
-                itemId={comment.id}
-                totals={comment.totals}
-                reacted={comment.reacted}
-                setReacted={(changedReacted) => {
-                  comment.reacted = changedReacted;
-                }}
-                hideCaakText
-                bottomTotals
-                textClassname={
-                  "text-[13px] font-medium text-13px tracking-[0.2px] leading-[16px] text-caak-nocturnal"
-                }
-                iconContainerClassname={"w-[24px] h-[24px] bg-transparent"}
-                iconColor={"text-caak-nocturnal"}
-                iconClassname={"text-[23px]"}
+            {isReplyInputActive && (
+              <ViewPostBlogAddComment
+                commentId={comment.id}
+                postId={postId}
+                setIsActive={setIsReplyInputActive}
+                rootContainerClassname={"mt-[16px]"}
+                containerClassname={"h-[94px]"}
+                reply={reply}
               />
-            </div>
+            )}
           </div>
 
           {children}
--- src/components/viewpost/CommentSection.js
+++ src/components/viewpost/CommentSection.js
@@ -1,20 +1,24 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ViewPostBlogAddComment from "../input/ViewPostBlogAddComment";
 import CommentCardNew from "../card/CommentCardNew";
 
 const CommentSection = ({ post, commentRef, jumpToCommentId }) => {
   const [commentInputValue, setCommentInputValue] = useState("");
-  const [reply, setReply] = useState({});
+  const [reply, setReply] = useState({
+    isReplying: false
+  });
 
   return (
     <div
       ref={commentRef}
-      className={"flex flex-col bg-white py-[4px] px-[8px] md:py-[16px] md:px-[29px] rounded-b-square"}
+      className={
+        "flex flex-col bg-white py-[4px] px-[8px] md:py-[16px] md:px-[29px] rounded-b-square"
+      }
     >
-      {/*User*/}
-
       {/*Add Comment section*/}
       <ViewPostBlogAddComment
+        inputClassname={"h-[97px]"}
+        containerClassname={"min-h-[135px]"}
         commentInputValue={commentInputValue}
         setCommentInputValue={setCommentInputValue}
         comments={post.comments.items}
@@ -26,10 +30,6 @@ const CommentSection = ({ post, commentRef, jumpToCommentId }) => {
 
       <CommentCardNew
         jumpToCommentId={jumpToCommentId}
-        commentInputValue={commentInputValue}
-        setCommentInputValue={setCommentInputValue}
-        reply={reply}
-        setReply={setReply}
         setup={{
           id: post.id,
           type: "POST",

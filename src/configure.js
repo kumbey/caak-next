cc0ff1376d700a5a922e94a5d918e1f8219938df
nano <nanoshdee@gmail.com>
2021-12-29 21:07:28 +0800

    Update configure.js

--- src/configure.js
+++ src/configure.js
@@ -1,6 +1,6 @@
 const Configure = {
     redirectSignIn: "https://localhost:3000/signInUp/federated/hook,https://main.d36nkdl3p18em1.amplifyapp.com/signInUp/federated/hook",
-    redirectSignOut: "https://localhost:3000/,https://main.d36nkdl3p18em1.amplifyapp.com/",
+    redirectSignOut: "https://localhost:3000,https://main.d36nkdl3p18em1.amplifyapp.com/",
     AESKey: "fL61wy76iB49HzbHhvcOkASBVlhXNjzS"
 }
 

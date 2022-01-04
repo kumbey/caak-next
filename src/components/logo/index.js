dbcd06dbba63623efd6d8cc50c8f30555e307341
weahforsage <sanjasapple@gmail.com>
2022-01-04 12:46:25 +0800

    fix: logo

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,33 +1,20 @@
 import Image from "next/image";
-import logoIcon from "../../../public/assets/images/New-Logo.svg";
-import logoText from "../../../public/assets/images/logo-text.svg";
+import logoIcon from "../../../public/New-Logo.svg";
 
 const Logo = ({ ...props }) => {
   return (
     <div
       {...props}
-      className={"relative flex flex-shrink-0 flex-row items-center w-[114px]"}
+      className={"relative flex flex-shrink-0 flex-row items-center w-[112px]"}
     >
-      <div className={"relative flex items-center justify-center w-[34px]"}>
         <Image
           src={logoIcon}
           className="cursor-pointer"
           alt="Caak Logo"
-          width={34}
+          width={112}
           height={34}
           objectFit="contain"
         />
-      </div>
-      <div className={"relative flex items-center justify-center w-[72px] ml-[5px]"}>
-        <Image
-          src={logoText}
-          className="cursor-pointer"
-          alt="Caak Logo"
-          width={72}
-          height={34}
-          objectFit="contain"
-        />
-      </div>
     </div>
   );
 };

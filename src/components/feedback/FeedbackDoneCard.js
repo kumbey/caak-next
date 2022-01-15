724b0f43ddc833beefc6dfcb4d3958732c3760f7
weahforsage <sanjasapple@gmail.com>
2022-01-15 19:44:29 +0800

    fix: feedback sent window timeout

--- src/components/feedback/FeedbackDoneCard.js
+++ src/components/feedback/FeedbackDoneCard.js
@@ -1,12 +1,29 @@
-import Image from "next/image";
 import smileHeart from "/public/assets/images/feedback/smileHeart.svg";
+import { useEffect } from "react";
 
-const FeedbackDoneCard = ({isOpen, setIsOpen}) => {
-  return isOpen && (
-    <div onClick={()=> setIsOpen(false)} className={"cursor-pointer w-[321px] h-[157px] feedBack flex flex-col items-center justify-center z-[10] fixed bottom-[78px] md:bottom-[90px] right-[24px] p-[25px]"}>
-      <img alt={""} src={smileHeart.src} height={38} width={38} />
-      <p className={"mt-[16px] text-white text-[22px] font-semibold"}>Танд баярлалаа</p>
-    </div>
+const FeedbackDoneCard = ({ isOpen, setIsOpen }) => {
+  const delay = 3;
+  useEffect(() => {
+    const timer = setTimeout(() => setIsOpen(false), delay * 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+  return (
+    isOpen && (
+      <div
+        onClick={() => setIsOpen(false)}
+        className={
+          "cursor-pointer w-[321px] h-[157px] feedBack flex flex-col items-center justify-center z-[10] fixed bottom-[78px] md:bottom-[90px] right-[24px] p-[25px]"
+        }
+      >
+        <img alt={""} src={smileHeart.src} height={38} width={38} />
+        <p className={"mt-[16px] text-white text-[22px] font-semibold"}>
+          Танд баярлалаа
+        </p>
+      </div>
+    )
   );
 };
 

d71efeca5a594364ca1c6c68f19adfdf096a6059
Purewee <purwee790@gmail.com>
2022-01-21 00:13:40 +0800

    Banner

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -1,35 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import khan from '../../../public/assets/images/khanbanner.jpg'
-import unitel from '../../../public/assets/images/unitel_banner.jpg'
-import golomt from '../../../public/assets/images/golomtbank.jpg'
-
-const data = [
-  {
-    image: khan,
-    link: "https://www.khanbank.com/",
-  },
-  {
-    image: unitel,
-    link: "https://www.unitel.mn/",
-  },
-  {
-    image: golomt,
-    link: "https://www.golomtbank.com/",
-  },
-];
+import Link from 'next/link';
+import React, {useEffect, useState} from 'react';
+import { API } from 'aws-amplify';
+import { getFileUrl } from '../../utility/Util';
+import { listBannersByType } from '../../graphql/queries';
 
 export default function Banner() {
+  const [banners, setBanners] = useState([])
   const [number, setNumber] = useState(0);
+
   useEffect(() => {
-    setNumber(Math.floor(Math.random() * data.length));
-  }, []);
+    const fetchBannersByType = async () => {
+        try{
+            const resp = await API.graphql({
+                query: listBannersByType,
+                variables: {
+                    type: 'A2',
+                },
+                authMode: 'AWS_IAM'
+            });
+            setBanners(resp.data.listBannersByType.items);
+            
+        }catch(ex){
+            console.log(ex);
+        }
+    }
+    fetchBannersByType()
+  },[])
+
+useEffect(() => {
+  setNumber(Math.floor(Math.random() * banners.length));
+}, []);
+
+console.log(number);
+
   return (
+    banners.length > 0 
+    ?
     <div className="sticky top-[74px]">
-      <a rel="noreferrer" href={data[number].link} target="_blank">
-        <div className="flex items-center justify-center w-full h-auto">
-          <img alt="" src={data[number].image.src} className="rounded-[8px]" />
-        </div>
-      </a>
+      {/* <a rel="noreferrer" href={banners[number]} target="_blank"> */}
+        <img alt="" src={getFileUrl(banners[number].pic1)} className="rounded-[8px] w-[300px] max-h-[400px]" />
+        
+      {/* </a> */}
     </div>
+    :
+    null
   );
 }
--- src/components/modalBanner/index.js
+++ src/components/modalBanner/index.js
@@ -1,27 +1,57 @@
 import Link from 'next/link';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import { API } from 'aws-amplify';
+import { getFileUrl } from '../../utility/Util';
+import { listBannersByType } from '../../graphql/queries';
 
 export default function ModalBanner({bannerOpen, setBannerOpen}) {
     const [modal, setModal] = useState(false)
+    const [banners, setBanners] = useState([])
+    const [number, setNumber] = useState(0);
+    
+
+    useEffect(() => {
+        const fetchBannersByType = async () => {
+            try{
+                const resp = await API.graphql({
+                    query: listBannersByType,
+                    variables: {
+                        type: 'A1',
+                    },
+                    authMode: 'AWS_IAM'
+                });
+                setBanners(resp.data.listBannersByType.items);
+                
+            }catch(ex){
+                console.log(ex);
+            }
+        }
+        fetchBannersByType()
+    },[])
+
+    useEffect(() => {
+        setNumber(Math.floor(Math.random() * banners.length));
+    }, []);
+
     return (
         bannerOpen ?
-        <div className="sticky bottom-8 left-8 w-[100px]">
-            <p onClick={() => setBannerOpen(false)} className='cursor-pointer'>close this shit</p>
-            <div onClick={() => setModal(true)} className='bg-[#FF6600] w-[100px] h-[100px] cursor-pointer mt-[10px]'>
-            
+        banners.length > 0
+        ?
+        <div className="sticky bottom-8 left-8 w-[100px] flex flex-col items-end">
+            <span onClick={() => setBannerOpen(false)} className='cursor-pointer icon-fi-rs-close bg-[#6C7392] p-2 rounded-full'/>
+            <div onClick={() => setModal(true)} className='border w-[100px] h-[100px] cursor-pointer mt-[10px]'>
+                <img alt="" src={getFileUrl(banners[number]?.pic2)} className="rounded-[8px] w-[100px] h-[100px] " />
             </div>
             {
                 modal
                 ?
-                <div className='popup_modal bg-transparent'>
+                <div className='popup_modal bg-transparent z-auto'>
                     <div className='popup_modal-banner'>
-                        <div className='flex flex-col'>
-                            <p onClick={() => setModal(false)} className='cursor-pointer'>close this shit</p>
+                        <div className='flex flex-col items-end'>
+                            <span onClick={() => setModal(false)}  className='cursor-pointer icon-fi-rs-close bg-[#6C7392] p-2 rounded-full'/>
                             <Link href={"https://playmo.mn/"}>
-                                <a target="_blank">
-                                    <div>
-                                        <p>asd</p>
-                                    </div>
+                                <a className='mt-[20px]' target="_blank">
+                                    <img alt="" src={getFileUrl(banners[number]?.pic1)} className="rounded-[8px] w-[500px]" />
                                 </a>
                             </Link>
                         </div>
@@ -31,6 +61,7 @@ export default function ModalBanner({bannerOpen, setBannerOpen}) {
                 null
             }
         </div>
+        :null
         :
         null
     )

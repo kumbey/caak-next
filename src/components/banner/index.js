59598306b652aa21b3ff52205acc7323805218c9
Purewee <purwee790@gmail.com>
2022-01-21 14:39:13 +0800

    banner

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -6,7 +6,7 @@ import { listBannersByType } from '../../graphql/queries';
 
 export default function Banner() {
   const [banners, setBanners] = useState([])
-  const [number, setNumber] = useState(0);
+  const [number, setNumber] = useState(null);
 
   useEffect(() => {
     const fetchBannersByType = async () => {
@@ -27,20 +27,22 @@ export default function Banner() {
     fetchBannersByType()
   },[])
 
-useEffect(() => {
-  setNumber(Math.floor(Math.random() * banners.length));
-}, []);
-
-console.log(number);
+  useEffect(() => {
+    setNumber(Math.floor(Math.random() * banners.length));
+    // eslint-disable-next-line
+  }, [banners]);
 
   return (
     banners.length > 0 
     ?
     <div className="sticky top-[74px]">
-      {/* <a rel="noreferrer" href={banners[number]} target="_blank"> */}
-        <img alt="" src={getFileUrl(banners[number].pic1)} className="rounded-[8px] w-[300px] max-h-[400px]" />
-        
-      {/* </a> */}
+      <a rel="noreferrer" href={JSON.parse(banners[number].meta).url ? JSON.parse(banners[number].meta).url : '/'} target="_blank">
+        <img alt="" src={getFileUrl(banners[number].pic1)} className={`rounded-[8px] ${JSON.parse(banners[number].meta).colors.border_color1 ? `border border-[${JSON.parse(banners[number].meta).colors.border_color1}]` : ""} b w-[300px] max-h-[400px] border`} />
+      </a>
+      <div className='flex flex-row items-center mt-[8px] ml-[10px]'>
+        <span className='icon-fi-rs-info text-[14px] text-[#6C7392]'/>
+        <p className='text-[13px] text-[#6C7392] ml-[4.6px]'>Caak ads</p>
+      </div>
     </div>
     :
     null
--- src/components/modalBanner/index.js
+++ src/components/modalBanner/index.js
@@ -7,8 +7,8 @@ import { listBannersByType } from '../../graphql/queries';
 export default function ModalBanner({bannerOpen, setBannerOpen}) {
     const [modal, setModal] = useState(false)
     const [banners, setBanners] = useState([])
-    const [number, setNumber] = useState(0);
-    
+    const [number, setNumber] = useState(null);
+    const [meta, setMeta] = useState();
 
     useEffect(() => {
         const fetchBannersByType = async () => {
@@ -31,29 +31,39 @@ export default function ModalBanner({bannerOpen, setBannerOpen}) {
 
     useEffect(() => {
         setNumber(Math.floor(Math.random() * banners.length));
+        // eslint-disable-next-line
+    }, [banners]);
+
+    useEffect(() => {
+        try{
+            if(banners.length > 0) {
+                const meta = JSON.parse(banners[number].meta)
+                setMeta(meta)
+            }
+        }catch(ex){
+            console.log(ex);
+        }
     }, []);
 
     return (
         bannerOpen ?
         banners.length > 0
         ?
-        <div className="sticky bottom-8 left-8 w-[100px] flex flex-col items-end">
-            <span onClick={() => setBannerOpen(false)} className='cursor-pointer icon-fi-rs-close bg-[#6C7392] p-2 rounded-full'/>
-            <div onClick={() => setModal(true)} className='border w-[100px] h-[100px] cursor-pointer mt-[10px]'>
-                <img alt="" src={getFileUrl(banners[number]?.pic2)} className="rounded-[8px] w-[100px] h-[100px] " />
+        <div className="sticky bottom-[50px] left-[50px] max-h-[150px] max-w-[170px] flex flex-row items-start">
+            <div onClick={() => setModal(true)} className={`rounded-[8px] ${JSON.parse(banners[number].meta).colors.border_color1 ? `border border-[${JSON.parse(banners[number].meta).colors.border_color1}]`: ''}  w-[150px] max-h-[150px] cursor-pointer`}>
+                <img alt="" src={getFileUrl(banners[number].pic2)} className="rounded-[8px] w-[150px] max-h-[150px] bg-white" />
             </div>
+            <span onClick={() => setBannerOpen(false)} className='cursor-pointer icon-fi-rs-close bg-[#9A9FB4] p-1 rounded-full ml-[8px]'/>
             {
                 modal
                 ?
-                <div className='popup_modal bg-transparent z-auto'>
+                <div className='popup_modal bg-transparent '>
                     <div className='popup_modal-banner'>
                         <div className='flex flex-col items-end'>
-                            <span onClick={() => setModal(false)}  className='cursor-pointer icon-fi-rs-close bg-[#6C7392] p-2 rounded-full'/>
-                            <Link href={"https://playmo.mn/"}>
-                                <a className='mt-[20px]' target="_blank">
-                                    <img alt="" src={getFileUrl(banners[number]?.pic1)} className="rounded-[8px] w-[500px]" />
-                                </a>
-                            </Link>
+                            <span onClick={() => setModal(false)}  className='cursor-pointer icon-fi-rs-close bg-[#9A9FB4] p-2 rounded-full'/>
+                            <a href={JSON.parse(banners[number].meta).url ? JSON.parse(banners[number].meta).url : '/'} className={`mt-[20px] rounded-[8px] ${JSON.parse(banners[number].meta).colors.border_color1 ? `border border-[${JSON.parse(banners[number].meta).colors.border_color1}]`: ''}`} target="_blank">
+                                <img alt="" src={getFileUrl(banners[number].pic1)} className="rounded-[8px] w-[500px] max-h-[500px]" />
+                            </a>
                         </div>
                     </div>
                 </div>

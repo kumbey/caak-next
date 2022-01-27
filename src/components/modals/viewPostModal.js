7cda56e9ad0d7d843d6faa2fd2a88e57608192dc
weahforsage <sanjasapple@gmail.com>
2022-01-27 12:25:53 +0800

    fix: for you page

--- src/components/modals/viewPostModal.js
+++ src/components/modals/viewPostModal.js
@@ -6,43 +6,44 @@ import { API } from "aws-amplify";
 import { Fragment, useEffect, useState } from "react";
 
 const ViewPostModal = () => {
-
   const router = useRouter();
   const { isModal } = router.query;
-  const [ssrData, setSsrData] = useState(null)
-  const [show, setShow] = useState(false)
+  const [ssrData, setSsrData] = useState(null);
+  const [show, setShow] = useState(false);
 
   const showModal = async () => {
-      
-      if(router.isReady && isModal){
-        if(router.query.viewPost === "post"){
-          setShow(true)
-          const resp = await ssrDataViewPost({ API ,Auth, query: router.query  })
-          setSsrData(resp.props.ssrData)   
-        }else{
-          setSsrData(null)
-          setShow(false)
+    if (router.isReady && isModal) {
+      if (router.query.viewPost === "post") {
+        setShow(true);
+        const resp = await ssrDataViewPost({
+          API,
+          Auth,
+          query: router.query,
+        });
+        if (resp.notFound) {
+          router.back();
         }
-      }else{
-        setSsrData(null)
-        setShow(false)
+        setSsrData(resp.props?.ssrData);
+      } else {
+        setSsrData(null);
+        setShow(false);
       }
-  }
+    } else {
+      setSsrData(null);
+      setShow(false);
+    }
+  };
 
   useEffect(() => {
-    showModal()
+    showModal();
     // eslint-disable-next-line
-  },[router.query])
+  }, [router.query]);
 
   return (
     <Fragment>
-      {
-        show ? (
-          ssrData ? <Post ssrData={ssrData}/> : "Loading"
-        ) : null
-      }
+      {show ? ssrData ? <Post ssrData={ssrData} /> : "Loading" : null}
     </Fragment>
-  )
+  );
 };
 
 export default ViewPostModal;

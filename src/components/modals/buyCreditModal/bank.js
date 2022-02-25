330dfed48dd2577c0c5444d6924a55372b04be9e
weahforsage <sanjasapple@gmail.com>
2022-02-25 15:58:12 +0800

    add: bagts picker modal

---
+++ src/components/modals/buyCreditModal/bank.js
@@ -0,0 +1,24 @@
+const Bank = ({ logo, selected, name, setSelected, index }) => {
+  return (
+    <div
+      className={
+        "last:ml-[13px] first:mr-[13px] py-[6px] selectBank bg-white flex flex-col justify-center items-center border-[1px] border-caak-titaniumwhite max-w-[172px]p px-[16px] w-full h-[72px] rounded-[8px] bg-white"
+      }
+    >
+      <img
+        alt={name}
+        className={"w-full h-full max-h-[29px] max-w-[140px] object-contain"}
+        src={logo.src}
+      />
+
+      <input
+        onChange={() => setSelected(index)}
+        className={"mt-[8px] w-[12px] h-[12px] ring-transparent"}
+        checked={selected}
+        type={"radio"}
+      />
+    </div>
+  );
+};
+
+export default Bank;

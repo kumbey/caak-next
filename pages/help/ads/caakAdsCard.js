39612f580388610acbf4f60af0c5c6dfc47d6297
Skratta <purwee790@gmail.com>
2022-02-25 17:56:25 +0800

    responsive

--- pages/help/ads/caakAdsCard.js
+++ pages/help/ads/caakAdsCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function CaakAdsCard({title, price}) {
   return (
-    <div style={{boxShadow: '0px 3px 6px #00000029'}} className='w-[350px] h-[354px] bg-white rounded-[12px] flex flex-col items-center'>
+    <div style={{boxShadow: '0px 3px 6px #00000029'}} className='w-full sm:w-[350px] h-[354px] bg-white rounded-[12px] flex flex-col items-center'>
         <div className='mt-[30px] flex flex-row items-center text-[#2B3A4C] text-[24px] font-semibold'><p className={`mr-[5px] ${title === "Hybrid" ? "text-[#FF6600]": "text-[#257CEE]"}`}>{title}</p>багц</div>
         <p className='mt-[13px] text-[46px] font-bold text-[#2B3A4C] h-[60px]'>{price}₮</p>
         <p className='mt-[14px] text-[#2B3A4C] font-semibold text-[#2B3A4C]'>Ашиглагдах Ads төрөл</p>

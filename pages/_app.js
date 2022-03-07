9e7eef29888f9718a6821a5ccb0fff7f96b7be0d
weahforsage <sanjasapple@gmail.com>
2022-03-07 11:02:49 +0800

    removed: alexa analytics (farewell alexa)

--- pages/_app.js
+++ pages/_app.js
@@ -64,12 +64,6 @@ const MyApp = ({ Component, pageProps }) => {
           });
         `}
       </Script>
-      <Script strategy="afterInteractive" id={"alex-analytics"}>
-        {`
-          _atrk_opts = { atrk_acct:"TDYMh1awVK00EN", domain:"caak.mn",dynamic: true};
-          (function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://certify-js.alexametrics.com/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
-        `}
-      </Script>
       <Script strategy="afterInteractive" id={"dx-tools"}>
         {`_type="text/javascript"
             window['_fs_debug'] = false;
